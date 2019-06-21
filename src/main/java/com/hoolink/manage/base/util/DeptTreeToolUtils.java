5ce705f79c837a4c54ece2946af2d453f6401aec
tonghao <1468935124@qq.com>
2019-06-21 17:26:47 +0800

    注释

--- src/main/java/com/hoolink/manage/base/util/DeptTreeToolUtils.java
+++ src/main/java/com/hoolink/manage/base/util/DeptTreeToolUtils.java
@@ -39,12 +39,12 @@ public class DeptTreeToolUtils {
             List<DepartmentAndUserTreeBO> rootUserList = new ArrayList<>();
             rootList.forEach(beanTree -> {
                 getChild(beanTree, map, hasAddUser, userMap, checkedList);
-                if (hasAddUser){
+              /*  if (hasAddUser){
                     List<SimpleDeptUserBO> userBOList = userMap.get(beanTree.getKey());
                     if (CollectionUtils.isNotEmpty(userBOList)){
                         buildUserList(rootUserList, beanTree, userBOList, checkedList);
                     }
-                }
+                }*/
             });
             rootList.addAll(rootUserList);
             return rootList;
