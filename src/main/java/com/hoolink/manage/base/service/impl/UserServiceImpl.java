f6df28800b2d775dcd7f74231b929868bb69133e
lijunling <294242871@qq.com>
2019-06-26 13:42:53 +0800

    上传头像 返回url

--- src/main/java/com/hoolink/manage/base/service/impl/UserServiceImpl.java
+++ src/main/java/com/hoolink/manage/base/service/impl/UserServiceImpl.java
@@ -1353,9 +1353,10 @@ public class UserServiceImpl implements UserService {
     }
 
 	@Override
-	public boolean uploadImage(MultipartFile multipartFile) {
+	public String uploadImage(MultipartFile multipartFile) {
 		BackBO<ObsBO> obsBo = abilityClient.uploadManager(multipartFile);
-		return updateImage(obsBo.getData().getId());
+		updateImage(obsBo.getData().getId());
+        return obsBo.getData().getObjectUrl();
 	}
 
 	@Override
