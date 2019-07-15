4b284cd690d4369b343b0cb851aa37d9f0703b39
韦敏 <15711036498@163.com>
2019-07-15 18:17:35 +0800

    提交优化

--- src/test/com/hoolink/manage/base/controller/TestController.java
+++ src/test/com/hoolink/manage/base/controller/TestController.java
@@ -1,18 +1,31 @@
 package com.hoolink.manage.base.controller;
 
+import com.hoolink.manage.base.ManagerApplication;
 import com.hoolink.sdk.bo.base.CurrentUserBO;
 import com.hoolink.sdk.utils.JSONUtils;
 import org.apache.servicecomb.swagger.invocation.context.ContextUtils;
 import org.apache.servicecomb.swagger.invocation.context.InvocationContext;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 测试父类
  * @author ：weimin
  */
+@RunWith(value = SpringRunner.class)
+@ActiveProfiles("dev")
+@SpringBootTest(classes = ManagerApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//配置事务的回滚,对数据库的增删改都会回滚,便于测试用例的循环利用
+@Rollback
+@Transactional
 public abstract class TestController {
 
     protected MockMvc mockMvc;
--- src/test/com/hoolink/manage/base/controller/server/MenuControllerTest.java
+++ src/test/com/hoolink/manage/base/controller/server/MenuControllerTest.java
@@ -1,39 +1,24 @@
 package com.hoolink.manage.base.controller.server;
 
 import com.alibaba.fastjson.TypeReference;
-import com.hoolink.manage.base.ManagerApplication;
 import com.hoolink.manage.base.controller.TestController;
 import com.hoolink.sdk.bo.BackBO;
 import com.hoolink.sdk.bo.edm.EdmMenuTreeBO;
 import com.hoolink.sdk.utils.JSONUtils;
-import com.hoolink.sdk.vo.BackVO;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
 
-@RunWith(value = SpringRunner.class)
-@ActiveProfiles("dev")
-@SpringBootTest(classes = ManagerApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//配置事务的回滚,对数据库的增删改都会回滚,便于测试用例的循环利用
-@Rollback
-@Transactional
 public class MenuControllerTest extends TestController {
 
     @Autowired
