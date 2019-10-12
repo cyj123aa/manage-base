9862c064956a6a2de96444d182699437f118a669
韦敏 <15711036498@163.com>
2019-10-12 09:54:57 +0800

    注释swagger

--- src/main/java/com/hoolink/manage/base/ManagerApplication.java
+++ src/main/java/com/hoolink/manage/base/ManagerApplication.java
@@ -7,13 +7,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author default
  */
 @EnableDiscoveryClient
-@EnableSwagger2
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableFeignClients(basePackages = {"com.hoolink.manage.base.consumer"})
